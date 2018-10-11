a0f09a9840b578d9ace02bbd2adc86610d79e664
Luca Leone <lucaleone@outlook.com>
2018-10-11 16:11:13 +0200

    added delete and edit functions

--- LucaLeone.WebCatalog.MVC/wwwroot/js/site.js
+++ LucaLeone.WebCatalog.MVC/wwwroot/js/site.js
@@ -1,4 +1,19 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function () {
+    // Wire up the Add button to send the new item to the server
+    $('.delete-Product-button').on('click', function (e) {
+        DeleteProduct(e.target);
+    });
+    $('.edit-Product-button').on('click', function (e) {
+        EditProduct(e.target);
+    });
+});
+function DeleteProduct(product) {
+    var productId = product.name;
+    console.log("DeleteProduct: " + productId);
+    window.location = '/DeleteProduct?id=' + productId;
+};
+function EditProduct(product) {
+    var productId = product.name;
+    console.log("EditProduct: " + productId);
+    window.location = '/EditProduct/' + productId;
+}
